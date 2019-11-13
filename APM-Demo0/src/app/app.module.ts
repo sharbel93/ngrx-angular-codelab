11848c89dd7dfa84e3668d6ccb2a02fba24b97b1
sharbel93 <sharbel@techfactory.ltd>
2019-11-13 16:22:24 +0300

    installing and initiating @ngrx/store-devtools

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { StoreModule } from '@ngrx/store';
 
 /* Feature Modules */
 import { UserModule } from './user/user.module';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -27,7 +29,12 @@ import { UserModule } from './user/user.module';
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
     AppRoutingModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'APM Demo App Devtools',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   declarations: [
     AppComponent,
