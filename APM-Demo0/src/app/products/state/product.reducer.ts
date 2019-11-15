fca5f25059c7757310dee9a623fb5c4234c0b0c6
sharbel93 <sharbel@techfactory.ltd>
2019-11-15 12:43:56 +0300

    installing @ngrx/effects and defining an Effect

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -64,6 +64,11 @@ export function reducer(state = initialState, action: ProductActions): ProductSt
           starRating: 0
         }
       };
+    case ProductActionTypes.LoadSuccess:
+      return {
+        ...state,
+        products: action.payload
+      };
     default:
       return state;
   }
